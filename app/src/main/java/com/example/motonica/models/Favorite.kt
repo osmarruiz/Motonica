d3472eab4020fad38b660f101cdc8be5374416a2
osmarruiz <osmarernesto17@gmail.com>
2024-05-24 21:24:16 -0600

    add motorcycle_image data class

--- app/src/main/java/com/example/motonica/models/Favorite.kt
+++
@@ -1,6 +0,0 @@
-package com.example.motonica.models
-
-data class Favorite(
-val user_id: Int,
-    val motorcycle_id: Int
-)
---
+++ app/src/main/java/com/example/motonica/models/motorcycle_image.kt
@@ -0,0 +1,7 @@
+package com.example.motonica.models
+
+data class motorcycle_image(
+    val url: String,
+    val is_hidden: Boolean,
+    val is_cover: Boolean
+)
